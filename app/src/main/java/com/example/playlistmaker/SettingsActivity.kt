9a83eb7d6a67eaf891c9a2337f2c880bc8879cdc
verman <reg@vrmn.ru>
2023-06-06 17:31:26 +0300

    Спринт 9/18 → Тема 1/5: Стили и темы → Урок 3/3
    Домашнее задание часть 3
    
    Оживил кнопки Share и Terms Of Use

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -21,26 +21,39 @@ class SettingsActivity : AppCompatActivity() {
         val shareButton = findViewById<LinearLayout>(R.id.shareButton)
 
         shareButton.setOnClickListener {
-            finish()
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                val shareButtonText = getResources().getString(R.string.shareButtonText)
+                putExtra(Intent.EXTRA_TEXT, shareButtonText)
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
 
         val supportButton = findViewById<LinearLayout>(R.id.supportButton)
 
         supportButton.setOnClickListener {
-            val message = "«Спасибо разработчикам и разработчицам за крутое приложение!»"
-            val title = "«Сообщение разработчикам и разработчицам приложения Playlist Maker»"
+//            val message = "«Спасибо разработчикам и разработчицам за крутое приложение!»"
+//            val title = "«Сообщение разработчикам и разработчицам приложения Playlist Maker»"
             val shareIntent = Intent(Intent.ACTION_SENDTO)
+            val supportButtonMessage = getResources().getString(R.string.supportButtonMessage)
+            val supportButtonText = getResources().getString(R.string.supportButtonText)
             shareIntent.data = Uri.parse("mailto:")
             shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("vermosti@ya.ru"))
-            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, title)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, supportButtonMessage)
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, supportButtonText)
             startActivity(shareIntent)
         }
 
         val termsOfUseButton = findViewById<LinearLayout>(R.id.termsOfUseButton)
 
         termsOfUseButton.setOnClickListener {
-            finish()
+            val shareIntent = Intent(Intent.ACTION_VIEW)
+            val termsOfUseArticle = getResources().getString(R.string.termsOfUseArticle)
+            shareIntent.data = Uri.parse(termsOfUseArticle)
+            startActivity(shareIntent)
         }
     }
 

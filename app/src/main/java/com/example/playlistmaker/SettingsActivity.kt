a9775968c1221a4ecbbe911b61e21096b7ee5fd9
verman <reg@vrmn.ru>
2023-06-06 15:16:24 +0300

    Спринт 9/18 → Тема 1/5: Стили и темы → Урок 3/3
    Домашнее задание часть 2
    
    Оживил кнопку Write to Support

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +17,36 @@ class SettingsActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             finish()
         }
+
+        val shareButton = findViewById<LinearLayout>(R.id.shareButton)
+
+        shareButton.setOnClickListener {
+            finish()
+        }
+
+        val supportButton = findViewById<LinearLayout>(R.id.supportButton)
+
+        supportButton.setOnClickListener {
+            val message = "«Спасибо разработчикам и разработчицам за крутое приложение!»"
+            val title = "«Сообщение разработчикам и разработчицам приложения Playlist Maker»"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("vermosti@ya.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, title)
+            startActivity(shareIntent)
+        }
+
+        val termsOfUseButton = findViewById<LinearLayout>(R.id.termsOfUseButton)
+
+        termsOfUseButton.setOnClickListener {
+            finish()
+        }
     }
 
 
 }
+
+
+
 

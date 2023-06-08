ab67ef5f166fd3d7dbac241ae716a4342b00399d
verman <reg@vrmn.ru>
2023-06-08 15:40:09 +0300

    Спринт 9/18 → Тема 1/5: Стили и темы → Урок 3/3
    Домашнее задание часть 4
    
    Сделал строку поиска и кнопку стирания запроса

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,75 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        val linearLayout = findViewById<FrameLayout>(R.id.container)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                if (s.isNullOrEmpty()) {
+//                    linearLayout.setBackgroundColor(getColor(R.color.prime_neutral))
+//                } else {
+//                    val input = s.toString()
+//                    if (isPrime(input.toInt())) {
+//                        linearLayout.setBackgroundColor(getColor(R.color.prime_positive))
+//                    } else {
+//                        linearLayout.setBackgroundColor(getColor(R.color.prime_negative))
+//                    }
+//                }
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun isPrime(number: Int): Boolean {
+        for (i in 2..number / 2) {
+            if (number % i == 0) {
+                return false
+            }
+        }
+        return true
     }
 }
+

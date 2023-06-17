ca5b1c89e1a2afe52437bcf2b7f4d6862e3add26
verman <reg@vrmn.ru>
2023-06-17 19:07:14 +0300

    7ая попытка сделать RecyclerView. Неудачная, но что ж делать

--- app/src/main/java/com/example/playlistmaker/ItemsViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/ItemsViewHolder.kt
@@ -8,12 +8,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 
-class ItemsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+abstract class ItemsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     private val compositionNameView: TextView = itemView.findViewById(R.id.compositionName)
     private val artistNameView: TextView = itemView.findViewById(R.id.artistName)
     private val durationView: TextView = itemView.findViewById(R.id.duration)
-    private val coverImageURLView: ImageView = itemView.findViewById(R.id.coverImageURL)
+    abstract val coverImageURLView: ImageView
 
     fun bind(item: Item) {
         compositionNameView.text = item.compositionName

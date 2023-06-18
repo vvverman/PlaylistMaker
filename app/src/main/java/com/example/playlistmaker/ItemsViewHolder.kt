a53bffb18e373b5cd7c51503a16e2ab1e35e5ab0
verman <reg@vrmn.ru>
2023-06-18 15:42:32 +0300

    дз 10ого спринта, добавил закруглённые углы вроде бы

--- app/src/main/java/com/example/playlistmaker/ItemsViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/ItemsViewHolder.kt
@@ -6,6 +6,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class ItemsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
@@ -19,10 +21,13 @@ class ItemsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         artistNameView.text = item.artistName
         durationView.text = item.duration
 
+        val requestOptions = RequestOptions().transform(RoundedCorners(20))
+
         Glide
             .with(itemView)
             .load(item.coverImageURL)
-            .placeholder(R.drawable.poster)
+            .apply(requestOptions)
+            .placeholder(R.drawable.placeholder_track)
             .transform(CenterCrop())
             .into(coverImageURLView)
     }

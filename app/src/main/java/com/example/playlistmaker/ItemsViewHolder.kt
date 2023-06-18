74862ebb061e578e05eb7b2b472e8a52f9efb1af
verman <reg@vrmn.ru>
2023-06-18 16:25:37 +0300

    дз 10ого спринта, закруглённые края, закруглились

--- app/src/main/java/com/example/playlistmaker/ItemsViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/ItemsViewHolder.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
@@ -21,14 +22,16 @@ class ItemsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         artistNameView.text = item.artistName
         durationView.text = item.duration
 
-        val requestOptions = RequestOptions().transform(RoundedCorners(20))
-
         Glide
             .with(itemView)
             .load(item.coverImageURL)
-            .apply(requestOptions)
+            .transform(
+                MultiTransformation(
+                    RoundedCorners(2),
+                    CenterCrop()
+                ))
             .placeholder(R.drawable.placeholder_track)
-            .transform(CenterCrop())
+
             .into(coverImageURLView)
     }
 }

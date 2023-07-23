e0e020d6297485641fe7331c8966742ed0cbfbe7
verman <reg@vrmn.ru>
2023-07-23 13:24:36 +0300

    11 спринт, оживили поиск по трекам с помощью Апи!! Чудо! продолжаю приводить в порядок

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import ITunesSearchApi
 import ItemsAdapter
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +16,15 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.example.playlistmaker.Item
+import com.example.playlistmaker.TracksResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 class SearchActivity : AppCompatActivity() {
 
@@ -32,7 +41,6 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val linearLayout = findViewById<FrameLayout>(R.id.container)
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
@@ -51,18 +59,18 @@ class SearchActivity : AppCompatActivity() {
 
                 valueInSearchString = s.toString()
                 clearButton.visibility = clearButtonVisibility(s)
+                // Вызов метода поиска музыкальных треков
+                searchTracks(s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
                 // empty
-
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
 
-        val itemsAdapter = ItemsAdapter(items)
-
         val itemsRecyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val itemsAdapter = ItemsAdapter()
         itemsRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         itemsRecyclerView.adapter = itemsAdapter
 
@@ -71,6 +79,7 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         const val REQUEST_TEXT = "REQUEST_TEXT"
     }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(REQUEST_TEXT, valueInSearchString)
@@ -84,7 +93,6 @@ class SearchActivity : AppCompatActivity() {
         Log.d("REQUEST_TEXT", "valueInSearchString before $valueInSearchString")
         valueInSearchString = savedInstanceState.getString(REQUEST_TEXT, "")
         Log.d("REQUEST_TEXT", "valueInSearchString after $valueInSearchString")
-
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -93,6 +101,41 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun searchTracks(searchTerm: String) {
+        // Создание экземпляра Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com") // Базовый URL API iTunes Search
+            .addConverterFactory(GsonConverterFactory.create()) // Используем Gson для сериализации/десериализации
+            .build()
+
+        // Создание объекта ITunesSearchApi с использованием экземпляра Retrofit
+        val iTunesSearchApi = retrofit.create(ITunesSearchApi::class.java)
+
+        // Вызов метода поиска музыкальных треков
+        val mediaType = "music"
+        val call = iTunesSearchApi.searchTracks(searchTerm, mediaType)
+
+        // Выполнение запроса асинхронно
+        call.enqueue(object : Callback<TracksResponse> {
+            override fun onResponse(call: Call<TracksResponse>, response: Response<TracksResponse>) {
+                if (response.isSuccessful) {
+                    val tracksResponse = response.body()
+                    tracksResponse?.items?.let { items ->
+                        // Обновите адаптер вашего RecyclerView с новыми данными из API
+                        val itemsAdapter = findViewById<RecyclerView>(R.id.recyclerView).adapter as? ItemsAdapter
+                        itemsAdapter?.updateItems(items)
+                    }
+                } else {
+                    // Обработка ошибки при запросе к API
+                }
+            }
+
+            override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                // Обработка ошибки при сбое запроса к API
+            }
+        })
     }
 
     private fun isPrime(number: Int): Boolean {
@@ -104,4 +147,3 @@ class SearchActivity : AppCompatActivity() {
         return true
     }
 }
-

913e0231bd549d69c16327128e20fa9cc8df25cf
verman <reg@vrmn.ru>
2023-07-30 21:33:26 +0300

    11 спринт никак не могу добиться того, чтоб показывалось сообщение при отключении от сети=(

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,6 +3,9 @@ package com.example.playlistmaker
 import ITunesSearchApi
 import ItemsAdapter
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -11,9 +14,9 @@ import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -69,7 +72,6 @@ class SearchActivity : AppCompatActivity() {
         val itemsAdapter = ItemsAdapter()
         itemsRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         itemsRecyclerView.adapter = itemsAdapter
-
     }
 
     companion object {
@@ -100,59 +102,77 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun searchTracks(searchTerm: String) {
-        // Создание экземпляра Retrofit
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://itunes.apple.com") // Базовый URL API iTunes Search
-            .addConverterFactory(GsonConverterFactory.create()) // Используем Gson для сериализации/десериализации
-            .build()
+        // Проверяем наличие интернет-соединения
+        val internetConnection = hasInternetConnection()
 
-        // Создание объекта ITunesSearchApi с использованием экземпляра Retrofit
-        val iTunesSearchApi = retrofit.create(ITunesSearchApi::class.java)
+        // При отсутствии интернет-соединения показываем сообщение о проблемах с сетью
+        updateNoNetworkMessageVisibility(internetConnection)
 
-        // Вызов метода поиска музыкальных треков
-        val mediaType = "music"
-        val call = iTunesSearchApi.searchTracks(searchTerm, mediaType)
+        if (internetConnection) {
+            // Создание экземпляра Retrofit
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://itunes.apple.com") // Базовый URL API iTunes Search
+                .addConverterFactory(GsonConverterFactory.create()) // Используем Gson для сериализации/десериализации
+                .build()
 
-        // Выполнение запроса асинхронно
-        call.enqueue(object : Callback<TracksResponse> {
-            override fun onResponse(call: Call<TracksResponse>, response: Response<TracksResponse>) {
-                if (response.isSuccessful) {
-                    val tracksResponse = response.body()
-                    tracksResponse?.items?.let { items ->
-                        // Обновите адаптер вашего RecyclerView с новыми данными из API
-                        val itemsAdapter = findViewById<RecyclerView>(R.id.recyclerView).adapter as? ItemsAdapter
-                        itemsAdapter?.updateItems(items)
-                        // Проверка наличия результатов и показ/скрытие плейсхолдера
-                        val placeholderImageView = findViewById<ImageView>(R.id.noSearchResultsImageView)
-                        val placeholderText = findViewById<TextView>(R.id.noSearchResultsText)
+            // Создание объекта ITunesSearchApi с использованием экземпляра Retrofit
+            val iTunesSearchApi = retrofit.create(ITunesSearchApi::class.java)
 
-                        if (items.isEmpty()) {
-                            // Показываем плейсхолдер, если результаты пусты
-                            placeholderImageView.visibility = View.VISIBLE
-                            placeholderText.visibility = View.VISIBLE
-                        } else {
-                            // Скрываем плейсхолдер, если есть результаты
-                            placeholderImageView.visibility = View.GONE
-                            placeholderText.visibility = View.GONE
+            // Вызов метода поиска музыкальных треков
+            val mediaType = "music"
+            val call = iTunesSearchApi.searchTracks(searchTerm, mediaType)
+
+            // Выполнение запроса асинхронно
+            call.enqueue(object : Callback<TracksResponse> {
+                override fun onResponse(call: Call<TracksResponse>, response: Response<TracksResponse>) {
+                    if (response.isSuccessful) {
+                        val tracksResponse = response.body()
+                        tracksResponse?.items?.let { items ->
+                            // Обновите адаптер вашего RecyclerView с новыми данными из API
+                            val itemsAdapter = findViewById<RecyclerView>(R.id.recyclerView).adapter as? ItemsAdapter
+                            itemsAdapter?.updateItems(items)
+                            // Проверка наличия результатов и показ/скрытие плейсхолдера
+                            val placeholderImageView = findViewById<FrameLayout>(R.id.noSearchResults)
+                            val communicationProblems = findViewById<FrameLayout>(R.id.communicationProblems)
+
+                            if (items.isEmpty()) {
+                                // Показываем плейсхолдер, если результаты пусты
+                                placeholderImageView.visibility = View.VISIBLE
+                            } else {
+                                // Скрываем плейсхолдер, если есть результаты
+                                placeholderImageView.visibility = View.GONE
+                            }
                         }
+                    } else {
+                        // Обработка ошибки при запросе к API
                     }
-                } else {
-                    // Обработка ошибки при запросе к API
                 }
-            }
 
-            override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
-                // Обработка ошибки при сбое запроса к API
-            }
-        })
+                override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                    // Обработка ошибки при сбое запроса к API
+                }
+            })
+        }
     }
 
-    private fun isPrime(number: Int): Boolean {
-        for (i in 2..number / 2) {
-            if (number % i == 0) {
-                return false
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+        if (connectivityManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                if (capabilities != null) {
+                    return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                }
+            } else {
+                val networkInfo = connectivityManager.activeNetworkInfo
+                return networkInfo != null && networkInfo.isConnected
             }
         }
-        return true
+        return false
+    }
+
+    private fun updateNoNetworkMessageVisibility(hasConnection: Boolean) {
+        val communicationProblems = findViewById<FrameLayout>(R.id.communicationProblems)
+        communicationProblems.visibility = if (hasConnection) View.GONE else View.VISIBLE
     }
 }

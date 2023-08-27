3992ddfc6eb81ff11bf122c3ae1f0102b4dd7a29
verman <reg@vrmn.ru>
2023-08-27 22:07:07 +0300

    12 спринт

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.example.playlistmaker
 
+import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +27,7 @@ class SettingsActivity : AppCompatActivity() {
         shareButton.setOnClickListener {
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
-                val shareButtonText = getResources().getString(R.string.shareButtonText)
+                val shareButtonText = resources.getString(R.string.shareButtonText)
                 putExtra(Intent.EXTRA_TEXT, shareButtonText)
                 type = "text/plain"
             }
@@ -35,11 +39,9 @@ class SettingsActivity : AppCompatActivity() {
         val supportButton = findViewById<LinearLayout>(R.id.supportButton)
 
         supportButton.setOnClickListener {
-//            val message = "«Спасибо разработчикам и разработчицам за крутое приложение!»"
-//            val title = "«Сообщение разработчикам и разработчицам приложения Playlist Maker»"
             val shareIntent = Intent(Intent.ACTION_SENDTO)
-            val supportButtonMessage = getResources().getString(R.string.supportButtonMessage)
-            val supportButtonText = getResources().getString(R.string.supportButtonText)
+            val supportButtonMessage = resources.getString(R.string.supportButtonMessage)
+            val supportButtonText = resources.getString(R.string.supportButtonText)
             shareIntent.data = Uri.parse("mailto:")
             shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("vermosti@ya.ru"))
             shareIntent.putExtra(Intent.EXTRA_TEXT, supportButtonMessage)
@@ -51,14 +53,57 @@ class SettingsActivity : AppCompatActivity() {
 
         termsOfUseButton.setOnClickListener {
             val shareIntent = Intent(Intent.ACTION_VIEW)
-            val termsOfUseArticle = getResources().getString(R.string.termsOfUseArticle)
+            val termsOfUseArticle = resources.getString(R.string.termsOfUseArticle)
             shareIntent.data = Uri.parse(termsOfUseArticle)
             startActivity(shareIntent)
         }
+
+        val themeSwitcher = findViewById<SwitchMaterial>(R.id.themeSwitcher)
+
+        themeSwitcher.setOnCheckedChangeListener { switcher, checked ->
+            (applicationContext as App).switchTheme(checked)
+        }
+
+    }
+}
+
+class App : Application() {
+    companion object {
+        private const val PREFS_NAME = "app_settings"
+        private const val DARK_THEME_ENABLED = "dark_theme_enabled"
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        loadThemeFromSharedPreferences()
+    }
 
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        saveThemeToSharedPreferences(darkThemeEnabled)
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+
+    private fun loadThemeFromSharedPreferences() {
+        val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val darkThemeEnabled = sharedPreferences.getBoolean(DARK_THEME_ENABLED, false)
+        switchTheme(darkThemeEnabled)
+    }
+
+    private fun saveThemeToSharedPreferences(darkThemeEnabled: Boolean) {
+        val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(DARK_THEME_ENABLED, darkThemeEnabled)
+        editor.apply()
+    }
 }
+
+
 
 
 

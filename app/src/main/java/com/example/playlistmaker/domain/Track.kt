9134d6a0855aa93f9689fff27340dd29b899dfe1
verman <reg@vrmn.ru>
2023-12-23 22:02:04 +0300

    15 спринт
    Разложил файлы по пакетам Data, Presentation, Domain. Создал файл с интерфейсами для работы с треками. Добавил комментарии

--- app/src/main/java/com/example/playlistmaker/domain/Track.kt
+++ app/src/main/java/com/example/playlistmaker/domain/Track.kt
@@ -2,24 +2,27 @@ package com.example.playlistmaker.domain
 
 import com.google.gson.annotations.SerializedName
 
+// Класс Track представляет модель данных для трека
 data class Track(
-    @SerializedName("ItemId") val itemId: Long,
-    @SerializedName("trackName") val compositionName: String,
-    @SerializedName("artistName") val artistName: String,
-    @SerializedName("trackTimeMillis") val durationInMillis: Long,
-    @SerializedName("artworkUrl100") val coverImageURL: String,
+    @SerializedName("ItemId") val itemId: Long, // Уникальный идентификатор трека
+    @SerializedName("trackName") val compositionName: String, // Название композиции
+    @SerializedName("artistName") val artistName: String, // Имя исполнителя
+    @SerializedName("trackTimeMillis") val durationInMillis: Long, // Продолжительность трека в миллисекундах
+    @SerializedName("artworkUrl100") val coverImageURL: String, // URL обложки трека
     @SerializedName("collectionName") val albumName: String?, // Название альбома (может быть null)
     @SerializedName("releaseDate") val releaseDate: String?, // Год релиза трека (может быть null)
     @SerializedName("primaryGenreName") val genre: String?, // Жанр трека (может быть null)
     @SerializedName("country") val country: String?, // Страна исполнителя (может быть null)
     @SerializedName("previewUrl") val previewUrl: String? // Ссылка на отрывок трека (может быть null)
 ) {
+    // Переопределение метода equals для сравнения объектов типа Track
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
         other as Track
 
+        // Сравнение всех полей объектов
         if (itemId != other.itemId) return false
         if (compositionName != other.compositionName) return false
         if (artistName != other.artistName) return false
@@ -34,6 +37,7 @@ data class Track(
         return true
     }
 
+    // Переопределение метода hashCode для генерации уникального хеш-кода объекта типа Track
     override fun hashCode(): Int {
         var result = itemId.hashCode()
         result = 31 * result + compositionName.hashCode()
@@ -46,8 +50,5 @@ data class Track(
         result = 31 * result + (country?.hashCode() ?: 0)
         result = 31 * result + (previewUrl?.hashCode() ?: 0)
         return result
-
     }
 }
-
-
--- app/src/main/java/com/example/playlistmaker/presentation/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/MediaLibraryActivity.kt
@@ -208,6 +208,8 @@ class MediaLibraryActivity : AppCompatActivity() {
         mediaPlayer = null
     }
 
+    
+
 //    fun formatTime(milliseconds: Int): String {
 //        val seconds = (milliseconds / 1000) % 60
 //        val minutes = (milliseconds / (1000 * 60)) % 60
@@ -225,6 +227,9 @@ class MediaLibraryActivity : AppCompatActivity() {
 //        // Повторно вызывайте метод updateCurrentTime() каждую секунду
 //        handler.postDelayed({ updateCurrentTime() }, 1000)
 //    }
+
+
+
 
     // Метод вызывается при восстановлении активности из фонового режима
     override fun onResume() {

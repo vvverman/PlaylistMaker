9eeaf37acee5d5e25ab0dea95ce08dd839b5f407
verman <reg@vrmn.ru>
2023-12-24 17:19:27 +0300

    15 спринт
    Вроде разложил, чо нашёл...

--- app/src/main/java/com/example/playlistmaker/domain/Track.kt
+++ app/src/main/java/com/example/playlistmaker/domain/Track.kt
@@ -15,40 +15,40 @@ data class Track(
     @SerializedName("country") val country: String?, // Страна исполнителя (может быть null)
     @SerializedName("previewUrl") val previewUrl: String? // Ссылка на отрывок трека (может быть null)
 ) {
-    // Переопределение метода equals для сравнения объектов типа Track
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Track
-
-        // Сравнение всех полей объектов
-        if (itemId != other.itemId) return false
-        if (compositionName != other.compositionName) return false
-        if (artistName != other.artistName) return false
-        if (durationInMillis != other.durationInMillis) return false
-        if (coverImageURL != other.coverImageURL) return false
-        if (albumName != other.albumName) return false
-        if (releaseDate != other.releaseDate) return false
-        if (genre != other.genre) return false
-        if (country != other.country) return false
-        if (previewUrl != other.previewUrl) return false
-
-        return true
-    }
-
-    // Переопределение метода hashCode для генерации уникального хеш-кода объекта типа Track
-    override fun hashCode(): Int {
-        var result = itemId.hashCode()
-        result = 31 * result + compositionName.hashCode()
-        result = 31 * result + artistName.hashCode()
-        result = 31 * result + durationInMillis.hashCode()
-        result = 31 * result + coverImageURL.hashCode()
-        result = 31 * result + (albumName?.hashCode() ?: 0)
-        result = 31 * result + (releaseDate?.hashCode() ?: 0)
-        result = 31 * result + (genre?.hashCode() ?: 0)
-        result = 31 * result + (country?.hashCode() ?: 0)
-        result = 31 * result + (previewUrl?.hashCode() ?: 0)
-        return result
-    }
+//    // Переопределение метода equals для сравнения объектов типа Track
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return false
+//
+//        other as Track
+//
+//        // Сравнение всех полей объектов
+//        if (itemId != other.itemId) return false
+//        if (compositionName != other.compositionName) return false
+//        if (artistName != other.artistName) return false
+//        if (durationInMillis != other.durationInMillis) return false
+//        if (coverImageURL != other.coverImageURL) return false
+//        if (albumName != other.albumName) return false
+//        if (releaseDate != other.releaseDate) return false
+//        if (genre != other.genre) return false
+//        if (country != other.country) return false
+//        if (previewUrl != other.previewUrl) return false
+//
+//        return true
+//    }
+//
+//    // Переопределение метода hashCode для генерации уникального хеш-кода объекта типа Track
+//    override fun hashCode(): Int {
+//        var result = itemId.hashCode()
+//        result = 31 * result + compositionName.hashCode()
+//        result = 31 * result + artistName.hashCode()
+//        result = 31 * result + durationInMillis.hashCode()
+//        result = 31 * result + coverImageURL.hashCode()
+//        result = 31 * result + (albumName?.hashCode() ?: 0)
+//        result = 31 * result + (releaseDate?.hashCode() ?: 0)
+//        result = 31 * result + (genre?.hashCode() ?: 0)
+//        result = 31 * result + (country?.hashCode() ?: 0)
+//        result = 31 * result + (previewUrl?.hashCode() ?: 0)
+//        return result
+//    }
 }
